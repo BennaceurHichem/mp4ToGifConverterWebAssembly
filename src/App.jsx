2965e31abde08e211741d1bc333a55e69ceb6a34
BennaceurHichem <gh_bennaceur@esi.dz>
2020-12-17 20:50:28 +0100

    load ffmpeg wasm files asynchroniselly successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {createFFmpeg, fetchFile} from '@ffmpeg/ffmpeg'
+
+
+const ffmpeg =  createFFmpeg({log:true})
 
 function App() {
   // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
+ const [ready,setReady] = useState(false)
+
+
+ const load =  async ()=>{
+
+  await ffmpeg.load()
+  setReady(true)
+ }
+
+
+ useEffect(()=>{
+  load()
+
+  
+ },[])
   // Return the App component.
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+
     </div>
   );
 }
